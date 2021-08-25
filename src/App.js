2d042843e4658a101d5987e297e6b74d14013af4
DICK Nicolas <dick.nicolas57@gmail.com>
2021-08-25 18:33:11 +0200

    feat: Base app files
    Create base todos-app file : index.html
    app.js : contain the app block like the header and the todos
    index.js : contain the render in the html of the app
    App.css & index.css contain the general style of the website and the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Todos from "./Components/Todos/Todos";
 
-function App() {
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <h1 className="title">Welcome on this awesome todo app! 📝</h1>
+        <h2 className="subtitle">Your todos are saved in the local storage</h2>
+      </div>
+      <Todos />
     </div>
   );
-}
+};
 
 export default App;
